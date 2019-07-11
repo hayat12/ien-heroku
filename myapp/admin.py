387c170d8ea5ad3d26f2a8ba19ba61703e45308f
hayat <hayat12>
2019-07-11 15:36:27 +0800

    add new project

---
+++ myapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from myapp.models import UserProfile, Event, Connection, MarketPlace, AdgendaInvites, EventInvites, UploadIMG
+
+# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Event)
+admin.site.register(Connection)
+admin.site.register(MarketPlace)
+admin.site.register(AdgendaInvites)
+admin.site.register(EventInvites)
+admin.site.register(UploadIMG)
